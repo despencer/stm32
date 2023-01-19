0e58f579c5f49c7ed1f858e19bd5ba37c1b0844b
despencer <despencer@mail.ru>
2023-01-19 22:59:43 +0300

    Added board and cpu specifications.

--- stm32.cmake
+++ stm32.cmake
@@ -1,1 +1,9 @@
+if(STM32_BOARD STREQUAL "BluePill")
+    set(STM32_CPU "STM32F103C8T6")
+endif()
+
+if(NOT DEFINED STM32_CPU)
+    message(FATAL_ERROR "STM32_CPU is not defined")
+endif()
+
 set(CMAKE_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/arm.cmake)
