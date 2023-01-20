1dd1947491ca535fa7e7dda6096406b38b93b98c
despencer <despencer@mail.ru>
2023-01-20 23:54:57 +0300

    Split compiler options into general STM32, cpu-specific and libopencm3 specific.

--- libopencm3/libopencm3.cmake
+++ libopencm3/libopencm3.cmake
@@ -1,28 +1,15 @@
+if(NOT DEFINED STM32_FAMILY)
+    message(FATAL_ERROR "STM32_FAMILY is not defined")
+endif()
+
+set(STM32_C_FLAGS ${STM32_C_FLAGS} -D${STM32_FAMILY})
 
 set(LINKER_FILE ${CMAKE_SOURCE_DIR}/stm32f103x8.ld)
 
 set(CMAKE_C_STANDARD 99)
 
 target_link_libraries(${EXECUTABLE} opencm3_stm32f1)
-target_compile_options(${EXECUTABLE} PRIVATE
--Os
--mthumb
--mcpu=cortex-m3
--msoft-float
--mfix-cortex-m3-ldrd
--Wextra
--Wshadow
--Wimplicit-function-declaration
--Wredundant-decls
--Wmissing-prototypes
--Wstrict-prototypes
--fno-common
--ffunction-sections
--fdata-sections
--Wall
--Wundef
--DSTM32F1
-)
+target_compile_options(${EXECUTABLE} PRIVATE ${STM32_C_FLAGS})
 
 target_link_options(${EXECUTABLE} PRIVATE
 --static
--- stm32.cmake
+++ stm32.cmake
@@ -8,4 +8,21 @@ if(NOT DEFINED STM32_CPU)
     message(FATAL_ERROR "STM32_CPU is not defined")
 endif()
 
+set(STM32_C_FLAGS ${STM32_C_FLAGS}
+   -Os  # size optimization
+   -mthumb  # 16-bit Thumb instruction set
+   -Wextra -Wshadow -Wimplicit-function-declaration -Wredundant-decls -Wmissing-prototypes -Wstrict-prototypes -Wall -Wundef  # warnings
+   -fno-common   # a way of treating uninitialized variables (put them to BSS)
+   -ffunction-sections -fdata-sections  # put each function and data item into its own section
+)
+
+if(STM32_CPU STREQUAL "STM32F103C8T6")
+   set(STM32_FAMILY "STM32F1")
+   set(STM32_C_FLAGS ${STM32_C_FLAGS}
+-mcpu=cortex-m3
+-msoft-float
+-mfix-cortex-m3-ldrd
+)
+endif()
+
 set(CMAKE_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/arm.cmake)
