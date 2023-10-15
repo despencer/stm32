17f2d68d556e83cf6dbe9bdcc49866fe33647768
despencer <despencer@mail.ru>
2023-10-15 20:06:26 +0300

    HAL generator for c files added.

--- libopencm3/opencm3hal/makehal.py
+++ libopencm3/opencm3hal/makehal.py
@@ -5,30 +5,41 @@ import yaml
 
 hal = "/opencm3hal"
 boards = "/boards"
-templates = [ "opencm3hal.h" ]
+templates = [ "opencm3hal.h", "opencm3hal.c" ]
 
 class Options:
     pass
 
-def getoptions(srcdir, optfile):
-#    with open(srcdir + '/settings.yaml') as fset:
-#        jset = yaml.load(fset, Loader=yaml.Loader)
-#    with open(srcdir + '/' + optfile) as fopt:
-#        jopt = yaml.load(fopt, Loader=yaml.Loader)
+def getfrequency(opt, jboard, jcpu):
+    for jmode in jcpu['general']['clock']:
+        if jmode['external'] == jboard['frequency']['cpu']:
+            opt.mcu.frequency = jmode['frequency']
+            opt.hal.clock = jmode['hal']
+            return
+    sys.stderr.write("Frequency not found\n")
+    exit(2)
+
+def getoptions(srcdir, boardfile):
+    with open("{0}{1}/{2}.board".format(srcdir, boards, boardfile)) as fboard:
+        jboard = yaml.load(fboard, Loader=yaml.Loader)['board']
+    with open("{0}{1}/{2}/{3}.mcu".format(srcdir, hal, jboard['cpu']['family'], jboard['cpu']['options'])) as fcpu:
+        jcpu = yaml.load(fcpu, Loader=yaml.Loader)
     opt = Options()
     opt.mcu = Options()
-    opt.mcu.frequency = 54000
-#    opt.greeting = jset['greeting'][jopt['greeting']]
+    opt.hal = Options()
+    getfrequency(opt, jboard, jcpu)
     return opt
 
 def main():
-    parser = argparse.ArgumentParser(description='Generates greetings file')
+    parser = argparse.ArgumentParser(description='Generates HAL file')
     parser.add_argument('srcdir', help='source dir')
     parser.add_argument('dstdir', help='destination dir')
     parser.add_argument('board', help='board yaml')
     args = parser.parse_args()
 
     options = getoptions(args.srcdir, args.board)
+    if options == None:
+        return 1
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.srcdir + hal))
     for tmpl in templates:
         with open(args.dstdir + '/' + tmpl, mode='w') as target:
