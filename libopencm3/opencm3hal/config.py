b578f4080c0b2ce599cf1d75b7e796a3ca2a009e
despencer <despencer@mail.ru>
2024-01-12 22:24:50 +0300

    Config parsing functionality moved into config.py

---
+++ libopencm3/opencm3hal/config.py
@@ -0,0 +1,52 @@
+import sys
+import yaml
+import output
+
+hal = "/opencm3hal"
+mappers = { 'output':output.Mapper() }
+boards = "/boards"
+
+class Options:
+    pass
+
+def getfrequency(opt, jboard, jcpu):
+    for jmode in jcpu['general']['clock']:
+        if jmode['external'] == jboard['frequency']['cpu']:
+            opt.mcu.frequency = jmode['frequency']
+            opt.hal.clock = jmode['hal']
+            return
+    sys.stderr.write("Frequency not found\n")
+    exit(2)
+
+def getoptions(haldir, boardfile):
+    with open("{0}{1}/{2}.board".format(haldir, boards, boardfile)) as fboard:
+        jboard = yaml.load(fboard, Loader=yaml.Loader)['board']
+    with open("{0}{1}/{2}/{3}.mcu".format(haldir, hal, jboard['cpu']['family'], jboard['cpu']['options'])) as fcpu:
+        jcpu = yaml.load(fcpu, Loader=yaml.Loader)
+    opt = Options()
+    opt.board = boardfile
+    opt.mcu = Options()
+    opt.mcu.family = jboard['cpu']['family']
+    opt.hal = Options()
+    getfrequency(opt, jboard, jcpu)
+    return opt
+
+def readconfig(cfgdir, options):
+    with open("{0}/board.config".format(cfgdir)) as fconfig:
+        jfconfig = yaml.load(fconfig, Loader=yaml.Loader)
+    jconfig = None
+    for jboard in jfconfig:
+        if jboard['board'] == options.board:
+            jconfig = jboard
+            break
+    if jconfig == None:
+        sys.stderr.write("Board not found in confguration\n")
+        exit(2)
+    cfg = Options()
+    cfg.mappers = []
+    for jmap in jconfig['mapping']:
+        mapper = mappers[jmap['type']]
+        mapper.addconfig(jmap, options)
+        if mapper not in cfg.mappers:
+            cfg.mappers.append(mapper)
+    return cfg
--- libopencm3/opencm3hal/makehal.py
+++ libopencm3/opencm3hal/makehal.py
@@ -1,35 +1,9 @@
 import jinja2
 import argparse
 import sys
-import yaml
+import config
 
-hal = "/opencm3hal"
-boards = "/boards"
 templates = [ "opencm3hal.h", "opencm3hal.c" ]
-
-class Options:
-    pass
-
-def getfrequency(opt, jboard, jcpu):
-    for jmode in jcpu['general']['clock']:
-        if jmode['external'] == jboard['frequency']['cpu']:
-            opt.mcu.frequency = jmode['frequency']
-            opt.hal.clock = jmode['hal']
-            return
-    sys.stderr.write("Frequency not found\n")
-    exit(2)
-
-def getoptions(haldir, boardfile):
-    with open("{0}{1}/{2}.board".format(haldir, boards, boardfile)) as fboard:
-        jboard = yaml.load(fboard, Loader=yaml.Loader)['board']
-    with open("{0}{1}/{2}/{3}.mcu".format(haldir, hal, jboard['cpu']['family'], jboard['cpu']['options'])) as fcpu:
-        jcpu = yaml.load(fcpu, Loader=yaml.Loader)
-    opt = Options()
-    opt.mcu = Options()
-    opt.mcu.family = jboard['cpu']['family']
-    opt.hal = Options()
-    getfrequency(opt, jboard, jcpu)
-    return opt
 
 def main():
     parser = argparse.ArgumentParser(description='Generates HAL file')
@@ -39,10 +13,11 @@ def main():
     parser.add_argument('board', help='board name')
     args = parser.parse_args()
 
-    options = getoptions(args.haldir, args.board)
+    options = config.getoptions(args.haldir, args.board)
     if options == None:
         return 1
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.haldir + hal))
+    cfg = config.readconfig(args.cfgdir, options)
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.haldir + config.hal))
     for tmpl in templates:
         with open(args.dstdir + '/' + tmpl, mode='w') as target:
             target.write(env.get_template(tmpl+'.jinja').render(options=options))
