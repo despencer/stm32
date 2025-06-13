69d9a0814a5c3f7628f104eec0a60c619510cbf9
despencer <despencer@mail.ru>
2025-06-13 17:46:38 +0300

    Channel type added as a parameter

--- cc/reader.py
+++ cc/reader.py
@@ -2,14 +2,21 @@
 
 import time
 
+def openpipe(args):
+    return open(args.channel, 'rb')
+
+def getchannel(args):
+    return {'pipe':openpipe}[args.chtype](args)
+
 def main():
     import argparse
 
     parser = argparse.ArgumentParser(description="Produces heartbeat")
-    parser.add_argument("channel", type=str, help="Communication channel")
+    parser.add_argument("chtype", type=str, help="Channel type")
+    parser.add_argument("channel", type=str, help="Channel name")
     args = parser.parse_args()
 
-    with open(args.channel, 'rb') as handle:
+    with getchannel(args) as handle:
         while True:
             msgbytes = handle.read(4)
             if len(msgbytes) == 0:
