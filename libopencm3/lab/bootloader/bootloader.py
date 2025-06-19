665eeae8a4932c0922516aaf64ccefe61328ba2c
despencer <despencer@mail.ru>
2025-06-19 14:36:28 +0300

    Bootloader GETID command added.

--- libopencm3/lab/bootloader/bootloader.py
+++ libopencm3/lab/bootloader/bootloader.py
@@ -3,22 +3,13 @@
 from threading import Thread, Lock
 
 STM32_START = 0x7F
-
-class Console:
-    def __init__(self):
-        self.lock = Lock()
-
-    def input(self):
-        return input(prompt)
-
-    def print(self, msg):
-        with self.lock:
-            print('\n'+msg+'\n'+prompt)
+STM32_ACK = 0x79
+STM32_GETCOMMAND = bytes.fromhex('00FF')
+STM32_GETIDCOMMAND = bytes.fromhex('02FD')
 
 class CommunicationProcessor:
-    def __init__(self, args, console):
+    def __init__(self, args):
         self.args = args
-        self.console = console
         import serial
         self.channel = serial.Serial(port=self.args.channel, baudrate=38400, parity=serial.PARITY_EVEN, bytesize=serial.EIGHTBITS,
                              stopbits=serial.STOPBITS_ONE)
@@ -44,8 +35,7 @@ class CommunicationProcessor:
         self.close()
 
 class CommandProcessor:
-    def __init__(self, args, console, channel):
-        self.console = console
+    def __init__(self, args, channel):
         self.channel = channel
         self.quit = False
         self.handlers = { 'version':self.cmdversion }
@@ -56,10 +46,39 @@ class CommandProcessor:
         else:
             self.console.print(f"Unrecognized command '{cmd}'")
 
-    def cmdversion(self):
+    def check_ack(self):
+        if int.from_bytes(self.channel.read(1)) != STM32_ACK:
+            print('No acknowledment from MCU')
+            return False
+        return True
+
+    def start(self):
         self.channel.write(STM32_START.to_bytes(1))
-        print(STM32_START.to_bytes(1))
-        print(self.channel.read(1))
+        print('Initializing bootloader interface')
+        return self.check_ack()
+
+    def cmdversion(self):
+        if not self.start():
+            return
+        self.channel.write(STM32_GETCOMMAND)
+        print('Sending GET command')
+        if not self.check_ack():
+            return
+        size = int.from_bytes(self.channel.read(1))
+        version = int.from_bytes(self.channel.read(1))
+        print(f'Bootloader version: {version:02X}')
+        self.channel.read(size)
+        self.check_ack()
+        self.channel.write(STM32_GETIDCOMMAND)
+        print('Sending GETID command')
+        if not self.check_ack():
+            return
+        size = int.from_bytes(self.channel.read(1)) - 1
+        did = int.from_bytes(self.channel.read(2), 'little')
+        print(f'Device ID: {did:04X}')
+        if size > 0:
+            self.channel.read(size)
+        self.check_ack()
 
 def main():
     import argparse
@@ -69,9 +88,8 @@ def main():
     parser.add_argument("command", type=str, help="Command")
     args = parser.parse_args()
 
-    console = Console()
-    with CommunicationProcessor(args, console) as comm:
-        CommandProcessor(args, console, comm).handle(args.command)
+    with CommunicationProcessor(args) as comm:
+        CommandProcessor(args, comm).handle(args.command)
 
 if __name__ == "__main__":
     main()
